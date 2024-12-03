49c43eb97d1d00ac566aa0fd7fc64118e826e699
Daniel Kimbel <dk@danielkimbel.com>
2024-12-02 22:30:35 -0700

    Solve Day 3 Part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,6 +2,10 @@ use std::fs;
 use regex::Regex;
 
 fn main() {
+    solve_part_2();
+}
+
+fn solve_part_1() {
     let content = fs::read_to_string("resources/input.txt").unwrap();
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let matches = re.captures_iter(&content);
@@ -13,4 +17,26 @@ fn main() {
     }
 
     println!("Part 1 solution: {sum}");
+}
+
+fn solve_part_2() {
+    let content = fs::read_to_string("resources/input.txt").unwrap();
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+    let matches = re.captures_iter(&content);
+
+    let mut sum = 0;
+    let mut enabled = true;
+    for capture in re.captures_iter(&content) {
+        let command = capture[0].to_string();
+        if command == "do()" {
+            enabled = true;
+        } else if command == "don't()" {
+            enabled = false;
+        } else if enabled {
+            let product = capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap();
+            sum += product;
+        }
+    }
+
+    println!("Part 2 solution: {sum}");
 }
